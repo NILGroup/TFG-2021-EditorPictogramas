dba11d1922c5b0225222a08e82b4b07ff4c7f43e
Alfon-III <altercer@ucm.es>
2021-05-07 02:18:00 +0200

    fix(BusquedaPictos) Reducido espacio + texto de apoyo historial vacío
    * Se ha arreglado la búsqueda para que al pulsar el botón de lupa haga la búsqueda
    * Se ha eliminado el espacio vacío si no se ha realizado ninguna búsqueda
    * Si el historial está vacío aparece un pequqño mensaje explicándote por qué.

--- Prototipo Tecnológico Inicial/Prototipo PictUp/CUADRICULA/src/views/API/arasaac.jsx
+++ Prototipo Tecnológico Inicial/Prototipo PictUp/CUADRICULA/src/views/API/arasaac.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import NILtraductor from './NILtraductor'
 
 import './pictoStyle.css';
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -11,6 +10,8 @@ class ARASAAC extends Component {
 		this.state = {
 			items: [],
 			query: '',
+			isLoaded: false,
+			selectedHistorial: false
 		};
 		this.cancel = ''
 	}
@@ -30,8 +31,8 @@ class ARASAAC extends Component {
 
 	//Cuando se hace onBlur
 	fetchInputQuery = (event) => {
-		this.sendQuery(event.target.value)
-		event.preventDefault();
+		this.sendQuery(this.state.query)
+		//event.preventDefault();
 	};
 	//Al pulsar Intro reaaliza la consulta
 	keyPress = (event) => {
@@ -44,8 +45,8 @@ class ARASAAC extends Component {
 	sendQuery(q) {
 		this.setState({
 			query: q
-		})
-		this.fetchResult(q)
+		}, () => this.fetchResult(q))
+		
 	}
 
 	//Envia información del pictograma seleccionado a la capa superior
@@ -57,11 +58,9 @@ class ARASAAC extends Component {
 
 	recentPictos = () => {
 		this.setState({
-			items: JSON.parse(localStorage.getItem("recentPictos"))
+			items: JSON.parse(localStorage.getItem("recentPictos")),
+			selectedHistorial: true,
 		})
-
-		console.log("hola")
-
 	}
 
 	sendSelectedPicto = (picto) => {
@@ -81,17 +80,75 @@ class ARASAAC extends Component {
 		this.props.sendData(picto);
 	}
 
-	getFraseFromTrad = (frase) =>{
+
+	getFraseFromTrad = (frase) => {
 		this.sendFrase(frase)
 	}
 
-	sendFrase = (frase) =>{
+	sendFrase = (frase) => {
 		this.props.sendFrase(frase)
 	}
 
 	sendSelectedCollection = (picto) => {
 		console.log(picto);
 		this.props.sendC(picto);
+	}
+
+	renderResultadoPicto() {
+
+		if (!this.state.isLoaded && !this.state.selectedHistorial) {
+			return ( null
+				// <div>
+				// 	<ul class="list-unstyled">
+				// 		<li>
+				// 			<p><em><span className="fas fa-search" /> Traduce una palabra a pictograma </em></p>
+				// 		</li>
+				// 		<li>
+				// 			<p><span className="fas fa-history" /><em> Consulta últimos pictos que has añadido al tablero</em></p>
+				// 		</li>
+				// 		<li>
+				// 			<p><span className="fas fa-folder-open" /><em> Añade un picto a una lista para usarlos más adelante</em></p>
+				// 		</li>
+				// 		<li>
+
+				// 		</li>
+				// 	</ul>
+				// </div>
+			)
+		}
+
+		if(this.state.selectedHistorial && this.state.items == null){
+			return(
+				<p className="fst-italic">No has colocado ningún pictograma aún. Aquí apareceran los útimos pictos que añadas al tablero 😉</p>
+			)
+		}
+
+		return (<div id="div1" className="row pl-4">
+			<div className="row row-cols-3 row-cols-md-5 g-4">
+
+				{this.state.items && !!this.state.items.length && this.state.items.map(item => (
+					<div key={item._id} className="card">
+						<img className="card-img-top"
+							src={'https://api.arasaac.org/api/pictograms/' + item._id}
+
+							sizes="40px" srcset="50px"
+						/>
+
+						<div className="card-body">
+							<h5 className="card-title text-center">{item.keywords[0].keyword}</h5>
+							<div className="btn-group" role="group">
+								<button className="btn-sm btn-primary" title="Añadir pictograma al tablero" onClick={() => this.sendSelectedPicto(item)}>
+									<i className="fas fa-plus"></i></button>
+								<button className="btn-sm btn-outline-primary" title="Añadir el pictograma a mis listas de pictogramas" onClick={() => this.sendSelectedCollection(item)}>
+									<span className="fas fa-folder-open"></span>
+								</button>
+							</div>
+						</div>
+					</div>
+				))}
+
+			</div>
+		</div>)
 	}
 
 
@@ -104,41 +161,16 @@ class ARASAAC extends Component {
 
 				<div className="input-group mb-3">
 					<div className="input-group-prepend">
-						<button className="btn btn-outline-primary" title="Buscar Pictograma">
+						<button className="btn btn-outline-primary" title="Buscar Pictograma" onClick={this.fetchInputQuery}>
 							<span className="fas fa-search"></span>
 						</button>
 						<button className="btn btn-outline-secondary" title="Pictogramas buscados recientemente" onClick={this.recentPictos}>
 							<span className="fas fa-history"></span>
 						</button>
 					</div>
-					<input type="text" className="form-control" placeholder='Busca a Picto' onBlur={this.fetchInputQuery} onKeyDown={this.keyPress} />
+					<input type="text" className="form-control" placeholder='Traducir palabra en picto'  onChange={e => this.setState({ query: e.target.value })} onKeyDown={this.keyPress} />
 				</div>
-				<div id="div1" className="row pl-4">
-					<div className="row row-cols-3 row-cols-md-5 g-4">
-
-						{this.state.items && !!this.state.items.length && this.state.items.map(item => (
-							<div key={item._id} className="card">
-								<img className="card-img-top"
-									src={'https://api.arasaac.org/api/pictograms/' + item._id}
-
-									sizes="40px" srcset="50px"
-								/>
-
-								<div className="card-body">
-									<h5 className="card-title text-center">{item.keywords[0].keyword}</h5>
-									<div className="btn-group" role="group">
-										<button className="btn-sm btn-primary" title="Añadir pictograma al tablero" onClick={() => this.sendSelectedPicto(item)}>
-											<i className="fas fa-plus"></i></button>
-										<button className="btn-sm btn-outline-primary" title="Añadir el pictograma a mis listas de pictogramas" onClick={() => this.sendSelectedCollection(item)}>
-											<span className="fas fa-folder-open"></span>
-										</button>
-									</div>
-								</div>
-							</div>
-						))}
-
-					</div>
-				</div>
+				{this.renderResultadoPicto()}
 			</div>
 
 		);
