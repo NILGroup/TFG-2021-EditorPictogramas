99f2a039a3553b93883e4bf2e67724c806648b58
Alfon-III <altercer@ucm.es>
2021-05-07 02:14:49 +0200

    feat(NavBar) Nuevos iconos añadidos
    * Placeholder del formulario
    * Otos proyectos grupo NIL
    
    TODO: Texto que acompañe a los botones cuando se pase el ratón por encima

--- Prototipo Tecnológico Inicial/Prototipo PictUp/CUADRICULA/src/views/Utilities/Navbar.jsx
+++ Prototipo Tecnológico Inicial/Prototipo PictUp/CUADRICULA/src/views/Utilities/Navbar.jsx
@@ -12,6 +12,18 @@ class Navbar extends Component {
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo"  style={{cursor:'default', fontSize: '30px'}} >Pict Up! <img src={"./img/PictUpIcon.png"} style={{marginTop: '-10px'}}  height="50" width="50" /></h1>
+                
+
+                <i class="fas fa-vote-yea"></i>
+                <div className="menu-icon" onClick={()=> window.open("https://chromedino.com/", "_blank")}><span style={{color: 'white'}}>
+                    <i style={{marginRight: '140px'}} className="fas fa-vote-yea"></i></span>
+                </div>
+                <div className="menu-icon" onClick={()=> window.open("http://nil.fdi.ucm.es/?q=aplicaciones/accesibilidad", "_blank")}><span style={{color: 'white'}}>
+                    <i style={{marginRight: '105px'}} className="fas fa-chalkboard-teacher"></i></span>
+                </div>
+                <div className="menu-icon" onClick={() => window.location.href = ("mailto:pictupdevalfon@gmail.com")}><span style={{color: 'white'}}>
+                    <i style={{marginRight: '70px'}} className="far fa-envelope-open"></i></span>
+                </div>
                 <div className="menu-icon" onClick={()=> window.open("https://www.instagram.com/pictupweb/", "_blank")}><span style={{color: 'white'}}>
                     <i style={{marginRight: '35px'}} className="fab fa-instagram"></i></span>
                 </div>
